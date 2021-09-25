ed99d94b9be4e8397d7f2439f344713a23f60722
Lauren Gatesman <55667325+lkgatesman@users.noreply.github.com>
2021-09-25 13:01:26 -0400

    Update LaborStrike.java

--- LaborStrike.java
+++ LaborStrike.java
@@ -6,16 +6,15 @@ public class LaborStrike{
 
         Scanner in = new Scanner(System.in);
 
-        int m = in.nextInt();
-        int n = in.nextInt();
+        int m = in.nextInt();   //the number of days in the calendar
+        int n = in.nextInt();   //the number of unions
 
-        int[] strikes = new int[n];
-
+        int[] strikes = new int[n]; //Array that holds the freqeuency with which each union i strikes at strikes[i].
         for (int i = 0; i < n; i++){
             strikes[i] = in.nextInt();
         }
 
-        int[][] calendar = new int[m][n];
+        int[][] calendar = new int[m][n];   //2D array that represents our calendar; each union a receives n days in its calendar
 
         //Set all the calendar days to 0, which indicates no strike on that day.
         for (int i = 0; i < m; i++){
@@ -25,35 +24,32 @@ public class LaborStrike{
         }
 
         //Set all the calendar days which have a strike to 1. 
-        int dayOfTheWeek = 0;
-        int day = 0;
+        int dayOfTheWeek = 0;   //tracks which day of the week it is (Sun=0, Mon=1, Tues=2, ..., Fri=6, Sat=7)
+        int day = 0;    //Tracks which day out of n days we are on.
         for (int union = 0; union < n; union++){
             day = 0;
             dayOfTheWeek = 0;
             while (day < m){
                 dayOfTheWeek++;
                 if ((day + 1) % strikes[union] == 0){
+                    //Strikes ONLY take place days that are not a Friday (6) or a Saturday (7), 
                     if (dayOfTheWeek != 6 && dayOfTheWeek != 7){
                         calendar[day][union] = 1;
                     }
                 }
+                //If the dayOfTheWeek is at 7 (Saturday), reset it to 0 (Sunday)
                 if (dayOfTheWeek == 7){
                     dayOfTheWeek = 0;
                 }
-                day ++;
+                day++;  //Move to the next day.
             }
         }
 
-        /*System.out.println(Arrays.toString(strikes));
-        for (int i = 0; i < m; i++){
-            System.out.println("Day " + (i+1) + Arrays.toString(calendar[i]));
-        }*/
 
         //Now look at each day. If it has at least one strike, increment noSubwayDays.
         int noSubwayDays = 0;
         for (int dayCounter = 0; dayCounter < m; dayCounter++){
             for (int unionCounter = 0; unionCounter < n; unionCounter++){
-
                 if (calendar[dayCounter][unionCounter] == 1){
                     noSubwayDays++;
                     break;  //move away from this day and onto the next for loop.
@@ -62,7 +58,7 @@ public class LaborStrike{
             }
         }
 
-        System.out.println(noSubwayDays);
+        System.out.println(noSubwayDays);   //print result.
 
         in.close();
 
